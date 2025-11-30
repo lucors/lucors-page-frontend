43ebc5eadccba7d49ad22849d8d6147c79e2e4f0
lucors <lucors.dev@gmail.com>
2025-11-30 22:32:37 +0300

    Хотфикс API_PATH

--- src/common/consts.js
+++ src/common/consts.js
@@ -2,4 +2,4 @@ export const TITLE_KEY = "title";
 
 export const DEFAULT_APP_ICON = "img/unknown-app.svg";
 
-export const API_PATH = (window.location.origin ?? "").includes("http") ? "http://localhost:3000" : window.location.origin + "/api";
+export const API_PATH = (window.location.origin ?? "").includes("http://") ? "http://localhost:3000" : window.location.origin + "/api";
